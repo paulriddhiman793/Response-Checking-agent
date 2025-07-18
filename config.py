2790f4a11846522ded58e6a410f949d452cdd868
paulriddhiman793 <paulriddhiman793@gmail.com>
2025-07-18 23:48:35 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# CSV File Configuration
+CSV_FILE_PATH = ""
+OUTPUT_CSV_PATH = ""
+
+# Groq Cloud API Key
+# Replace with your API key
+GROQ_API_KEY = ""
+MODEL_NAME = "meta-llama/llama-4-scout-17b-16e-instruct"
